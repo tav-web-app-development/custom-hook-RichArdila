6ca8663ea54787da5688af753e0ff1f4dd1e91f7
Richard Ardila <richardardilaz17@hotmail.com>
2024-05-07 19:30:20 -0400

    Added useTime Hook and Clock component

---
+++ src/useTime.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+
+function useTime() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return time;
+}
+
+export default useTime;
